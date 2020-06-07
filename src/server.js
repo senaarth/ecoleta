ebd74b9cdb1c7d070f699271ce6aec50e6be17d9
senaarth <senaarth@gmail.com>
2020-06-07 16:00:56 -0300

    setting search results and point created screen, some fixes in search results page

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ const db = require("./database/db.js")
 // Setting Public Page
 server.use(express.static("public"))
 
+server.use(express.urlencoded({ extended: true }))
+
 // Template End
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
@@ -26,12 +28,54 @@ server.get("/create-point", (req, res) => {
 })
 
 server.post("/savepoint", (req, res) => {
-    return res.send("ok")
+    // req.body = form body
+    // console.log(req.body)
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?, ?, ?, ?, ?, ?, ?);
+    `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            return console.log(err)
+        }
+        console.log("Cadastro Sucedido")
+        console.log(this)
+        return res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
 })
 
 server.get("/search-results", (req, res) => {
+    const search = req.query.search
+    if (search == "") {
+        // Empty Search
+        return res.render("search-results.html", { total: 0 })
+    }
+
+
+
+
     // get the data from the db
-    db.all(`SELECT * FROM places`, function(err, rows) {
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
         if (err) {
             return console.log(err)
         }
